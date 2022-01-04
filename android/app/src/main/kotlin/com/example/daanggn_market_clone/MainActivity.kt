dd3e233f6d2d8cfb9b7dac7f354b8977960b4c86
jobum97 <brian654321@naver.com>
2022-01-04 14:41:24 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/daanggn_market_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.daanggn_market_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const usedItemPage(title: 'Daanggn Market'),
+    );
+  }
+}
+
+final List<String> items = <String>["GTX 1060","로지텍 마우스","한우 갈빗살"];
+
+class usedItemPage extends StatefulWidget{
+  const usedItemPage({Key? key, required this.title}) : super(key: key); //컴파일타임에 결정됨
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => _usedItemPageState();
+}
+class _usedItemPageState extends State<usedItemPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+}
+
+// ListView.builder(
+// padding: const EdgeInsets.all(8),
+// itemCount: items.length,
+// itemBuilder: (BuildContext context,int index){
+// return Container(
+// height: 50,
+// color: Colors.green,
+// child: Center(child: Text('Entry ${items[index]}')),
+// );
+// }
+// );
